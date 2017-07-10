f0a7e0af78fa7b96b9fed333bb6a88c07f50f731
Claudio Traspadini Oliveira <caltras@gmail.com>
2017-07-10 16:36:31 -0300

    table script

--- script_table.sql
+++ script_table.sql
@@ -1,5 +1,9 @@
+CREATE DATABASE luxysales;
+GRANT ALL ON luxysales.* TO 'luxysales' IDENTIFIED BY 'luxysales';
+
 CREATE TABLE `luxysales`.`words` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `word` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `word_UNIQUE` (`word` ASC));
+  UNIQUE INDEX `word_UNIQUE` (`word` ASC)
+ );
